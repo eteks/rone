c7a973da9bd716d860991c56c0dca69dd44deb4b
cjanarthanangit <janaece_12@rocketmail.com>
2019-08-22 17:03:32 +0530

    Alignment Changes for Privacy policy and terms and condition.

--- application/views/default/layout/content/privacy.php
+++ application/views/default/layout/content/privacy.php
@@ -3,8 +3,8 @@
 <div>
 <div>
 <div class="red-subtitle top-red-subtitle" style="margin:0px 0 0 0"><?php echo $content->pages_title; ?></div>
-<div class="profile_back">
-<div class="container">
+<div class="profile_back" style="padding:0px !important;">
+<div class="container" style="letter-spacing: 1.3px; text-align: justify; line-height: 1.7;">
 <div class="db-rightinfo db-rightinfo-inner" style="width:100%; margin:0px 0 0 0">
 <div class="home-signpost-content" style="min-height:330px;">
 
@@ -21,6 +21,138 @@
 ?>
 </p>
 </div>
+
+<!-- <div class="all-detail" style="padding-top:0px; padding-bottom:15px;">
+<p>Hireadronepilot ("We") manage the website http://www.hireadronepilot.com.au (the "Service").The following describes our policies regarding the collection, use and disclosure of personal information when you use our service.We will not use or share your information with anyone except as described in this Privacy Policy.We may use your personal information to provide and improve the service. By using the Service, you agree that we will collect and use the information provided in accordance with this policy.Unless otherwise defined in this Privacy Policy, the terms used in this Privacy Policy have the same meanings as the Terms and Conditions of Use, you find here.</p>
+<div class="inner-deti" style="padding:60px 0 60px 0;">Collection & Use Of Information</div>
+<div class="only-question" >
+<ul><li>While using our service, we may ask you to provide us with certain personally identifiable information that we may use to contact or identify you. Identification information may include, but is not limited to, your e-mail address, name, ID number, telephone number, postal address, other information ("Personal Information").</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Log Data</div>
+<div class="only-question" >
+<ul>
+<li>Our servers collect information that your browser sends each time you visit our Service ("Log Data"). Log data can collect information such as your IP address ("IP"), type of browser, browser version, pages of our service you visit, time and date of your visit, time spent on those pages And other statistics.</li>
+<li>In addition, we may use third-party services such as Google Analytics which collects, monitors and analyzes this type of information in order to increase the functionality of our service. These service providers have their own privacy policies versus the way they use such information.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Cookies</div>
+<div class="only-question" >
+<ul>
+<li>Cookies are files with small amounts of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your computer's hard drive.</li>
+<li>Our servers may use "cookies" to collect information. You can set your browser to reject all cookies or to indicate when a cookie is sent. However, if you do not accept cookies, you may not be able to use some parts of our service.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Service Providers</div>
+<div class="only-question" >
+<ul>
+<li>We may use the services of third parties to facilitate our service, to provide the service on our behalf, to perform services related to the service or to help us analyze how our service is used.</li>
+<li>These third parties have access to your personal information only to carry out these tasks on our behalf and are required not to disclose or use it for any other purpose.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Communications</div>
+<div class="only-question" >
+<ul>
+<li>We may use your personal information to contact you with newsletters, promotional material and other information that may be of interest to you. You may choose not to receive any or all of these communications from us by following the unsubscribe link or the instructions provided in any email we send or by contacting us.</li>
+</ul>
+</div>
+
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Compliance With Laws</div>
+<div class="only-question" >
+<ul>
+<li>We will release your personal information when required by law or if we believe such action is necessary to comply with the law or to protect the safety or integrity of our service.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Security</div>
+<div class="only-question" >
+<ul>
+<li>The security of your personal information is important to us, but remember that no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we can not guarantee your absolute security.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">International Transfer Of Your Information</div>
+<div class="only-question" >
+<ul>
+<li>Your information, including personal information, may be transferred to - and maintained at - computers that are located outside your country where data protection laws may be different from those in your country.</li>
+<li>If you are outside Australia and choose to provide information to us, please note that we transfer this information, including your personal information, to Australia and process it there.</li>
+<li>Your consent to this Privacy Policy represents acceptance of the transfer.</li>
+</ul>
+</div>
+
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Hyperlinks To Other Pages</div>
+<div class="only-question" >
+<ul>
+<li>Our Service may contain hyperlinks to other sites not operated by us. If you click on a link from a third party, you will be directed to the third party site. We recommend that you review the Privacy Policy of each site you visit.</li>
+<li>We have no control over the sites or services of third parties and assume no responsibility for the content, policies or privacy practices of these.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Privacy Of Minors</div>
+<div class="only-question" >
+<ul>
+<li>You must not allow minors to register and use our service. ("Minors").</li>
+<li>We do not collect personally identifiable information from minors. If you are a parent or guardian and have knowledge that your children have provided us with personal information, please contact us. If we become aware that we have collected personal information from minors without the consent of their parents, we will take steps to remove such information from our servers.</li>
+</ul>
+</div>
+
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Changes to this Privacy Policy</div>
+<div class="only-question" >
+<ul>
+<li>We may update our privacy policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.You are advised to review this Privacy Policy periodically for any changes to the service. Changes to this Privacy Policy are effective when posted on this page.</li>
+</ul>
+</div>
+
+<div class="inner-deti" style="padding:60px 0 60px 0;">Contact Us</div>
+<p>If you have any questions about this Privacy Policy, please <a href="<?php echo base_url();?>/contact_us">contact us</a></p>
+</div> -->
+
+
+
+			<!-- 	<div class="all-detail"  padding-bottom:15px;">
+<p>Hireadronepilot ("We") manage the website http://www.hireadronepilot.com.au (the "Service").The following describes our policies regarding the collection, use and disclosure of personal information when you use our service.We will not use or share your information with anyone except as described in this Privacy Policy.We may use your personal information to provide and improve the service. By using the Service, you agree that we will collect and use the information provided in accordance with this policy.Unless otherwise defined in this Privacy Policy, the terms used in this Privacy Policy have the same meanings as the Terms and Conditions of Use, you find here.</p>
+<div class="inner-deti"  0 60px 0;">Collection & Use Of Information</div>
+<div class="only-question" >
+<ul><li>While using our service, we may ask you to provide us with certain personally identifiable information that we may use to contact or identify you. Identification information may include, but is not limited to, your e-mail address, name, ID number, telephone number, postal address, other information ("Personal Information").</li>
+<div class="inner-deti"  0 60px 0;">Log Data</div>
+<li>Our servers collect information that your browser sends each time you visit our Service ("Log Data"). Log data can collect information such as your IP address ("IP"), type of browser, browser version, pages of our service you visit, time and date of your visit, time spent on those pages And other statistics.</li>
+<li>In addition, we may use third-party services such as Google Analytics which collects, monitors and analyzes this type of information in order to increase the functionality of our service. These service providers have their own privacy policies versus the way they use such information.</li>
+<div class="inner-deti"  0 60px 0;">Cookies</div>
+<li>Cookies are files with small amounts of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your computer's hard drive.</li>
+<li>Our servers may use "cookies" to collect information. You can set your browser to reject all cookies or to indicate when a cookie is sent. However, if you do not accept cookies, you may not be able to use some parts of our service.</li>
+<div class="inner-deti"  0 60px 0;">Service Providers</div>
+<li>We may use the services of third parties to facilitate our service, to provide the service on our behalf, to perform services related to the service or to help us analyze how our service is used.</li>
+<li>These third parties have access to your personal information only to carry out these tasks on our behalf and are required not to disclose or use it for any other purpose.</li>
+<div class="inner-deti"  0 60px 0;">Communications</div>
+<li>We may use your personal information to contact you with newsletters, promotional material and other information that may be of interest to you. You may choose not to receive any or all of these communications from us by following the unsubscribe link or the instructions provided in any email we send or by contacting us.</li>
+<div class="inner-deti"  0 60px 0;">Compliance With Laws</div>
+<li>We will release your personal information when required by law or if we believe such action is necessary to comply with the law or to protect the safety or integrity of our service.</li>
+<div class="inner-deti"  0 60px 0;">Security</div>
+<li>The security of your personal information is important to us, but remember that no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we can not guarantee your absolute security.</li>
+<div class="inner-deti"  0 60px 0;">International Transfer Of Your Information</div>
+<li>Your information, including personal information, may be transferred to - and maintained at - computers that are located outside your country where data protection laws may be different from those in your country.</li>
+<li>If you are outside Australia and choose to provide information to us, please note that we transfer this information, including your personal information, to Australia and process it there.</li>
+<li>Your consent to this Privacy Policy represents acceptance of the transfer.</li>
+<div class="inner-deti"  0 60px 0;">Hyperlinks To Other Pages</div>
+<li>Our Service may contain hyperlinks to other sites not operated by us. If you click on a link from a third party, you will be directed to the third party site. We recommend that you review the Privacy Policy of each site you visit.</li>
+<li>We have no control over the sites or services of third parties and assume no responsibility for the content, policies or privacy practices of these.</li>
+<div class="inner-deti"  0 60px 0;">Privacy Of Minors</div>
+<li>You must not allow minors to register and use our service. ("Minors").</li>
+<li>We do not collect personally identifiable information from minors. If you are a parent or guardian and have knowledge that your children have provided us with personal information, please contact us. If we become aware that we have collected personal information from minors without the consent of their parents, we will take steps to remove such information from our servers.</li>
+<div class="inner-deti"  0 60px 0;">Changes to this Privacy Policy</div>
+<li>We may update our privacy policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.You are advised to review this Privacy Policy periodically for any changes to the service. Changes to this Privacy Policy are effective when posted on this page.</li>
+<div class="inner-deti"  0 60px 0;">Contact Us</div>
+<p>If you have any questions about this Privacy Policy, please <a href="<?php echo base_url();?>/contact_us">contact us</a></p>
+</div>			 -->
+
+
 </div>
 </div>
 <div class="clear"></div>
--- application/views/default/layout/content/terms_of_use.php
+++ application/views/default/layout/content/terms_of_use.php
@@ -4,7 +4,7 @@
 <div>
 <div class="red-subtitle top-red-subtitle" style="margin:0px 0 0 0"><?php echo $content->pages_title; ?></div>
 <div class="profile_back" style="padding:0px !important;">
-<div class="container">
+<div class="container" style="letter-spacing: 1.3px; text-align: justify; line-height: 1.7;">
 <div class="db-rightinfo db-rightinfo-inner" style="width:100%; margin:0px 0 0 0">
 <div class="home-signpost-content" style="min-height:330px;">
 
@@ -21,7 +21,44 @@
 ?>
 </p>
 </div>
-</div>
+
+<!-- this -->
+
+<!-- <div class="all-detail" style="padding-top:0px; padding-bottom:15px;">
+                            <p>This agreement describes the terms and conditions (the "Terms and Conditions") applicable to the use of the services offered within the site Hireadronepilot.com.au ( "Hireadronepilot", "Hireadronepilot" The "site" or "platform"). Any person who wishes to access and / or use the site or services may do so subject to the General Terms and Conditions, along with all other policies and principles that govern Hireadronepilot.com.au and are hereby incorporated by reference.</p>
+                            <br/>
+                            <p>ANY PERSON DOES NOT ACCEPT THESE GENERAL TERMS AND CONDITIONS, WHICH HAVE A MANDATORY AND BINDING CHARACTER, SHOULD BE ABSENT OF USING THE SITE AND / OR THE SERVICES. </p>
+                            <br/>
+                            <p>The User must read, understand and accept all the conditions set out in the General Terms and Conditions and Privacy Policy as well as other documents incorporated therein by reference, prior to its registration as a user Hireadronepilot.com.au</p>
+                            <div class="inner-deti" style="padding:60px 0 60px 0;">Capacity</div>
+                            <div class="only-question" >
+                            <ul><li>The Services are only available to persons who have legal capacity to hire. The services may not be used by persons who do not have that capacity, minors or Hireadronepilot.com.au Users who have been temporarily suspended or permanently disabled. If you are registering a User as a Company, you must be able to contract on behalf of such entity and bind it under the terms of this Agreement.</li></ul></div>
+                            <div class="inner-deti" style="padding:60px 0 60px 0;">Object of Hireadronepilot Service</div>
+                            <div class="only-question" >
+                            <ul><li>Hireadronepilot provides a platform to connect those who need to do a task, ("Task Providers") with those who are willing to do the work ("Droners").</li>
+                            <li>The term "user" or "users" in this agreement includes Task Providers of Tasks, Droners and any other person who visits or sees the Hireadronepilot.com.au Platform.</li>
+                            <li>Users get an account Hireadronepilot.com.au when he/she completes the registration form in all fields with valid data Hireadronepilot.com.au Platform</li>
+                            <li>Hireadronepilot only provides the platform which allows the Task Providers to find a Droners to provide a service, hireadronepilot accepts no responsibility for any aspect of the transaction between the parties, including, but not limited to: the description of the services Offered and efficiency in the provision of these. Hireadronepilot has no obligation to users in any dispute that may arise between the Task Providers and the hireadronepilot.</li>
+                            <li>All information related to the services to be performed is provided by the Users. Hireadronepilot has no ability or responsibility to review, approve or verify any information provided by Users on the platform.</li>
+                            <li>You expressly agree that Hireadronepilot has no responsibility and makes no warranty as to the truthfulness or accuracy of the information provided by users (including Droners and Task Providers), including, but not limited to, the ability of Droners to perform The tasks, or the honesty or accuracy of the information provided by the Task Providers or their ability to pay for the services requested.</li>
+                            </ul></div>
+                            <div class="inner-deti" style="padding:60px 0 60px 0;">Description of Hireadronepilot Service</div>
+                             <div class="only-question" >
+                            <ul><li>The Hireadronepilot Service will be reviewed and updated from time to time. In addition to the points listed below, some parts of Hireadronepilot Service may be described separately on the Hireadronepilot Platform.</li>
+                            <li>A Task Provider who requires the completion of a task or work: (1) creates and has Hireadronepilot and (2) publishes the assignment, including an accurate and complete description of the service to be provided (including, but not limited to: Offers to the Droners) ("published task"). Published tasks are subject to other conditions set forth below, for "Posting Tasks" and "Offering".</li>
+                            <li>A Droners, with the capacity to render the services: (1) creates an account in hireadronepilot, (2) registers as Droners and (3) revises the tasks published by the Task Providers.</li>
+                            <li>If a Hireadronepilot wishes to render the services, he must make an offer to the published task ("Offer"). By "Bidding", the hireadronepilot confirms that he / she is legally empowered and has the ability to provide the services described in the published task. "Offer" is subject to other conditions set forth below, for "Posting of Tasks" and "Offering".</li>
+                            <li>If a Task Provider modifies or updates a task after it has been posted on the hireadronepilot Platform and before an offer has been accepted, hireadronepilot may at its discretion cancel all bids that were made for that task prior to the modification and recommence The supply process.</li>
+                            <li>If a Task Provider wishes to accept a Droner`s offer, the Droners must do so using the "Offer" feature on the hireadronepilot platform.</li>
+                            <li>Droners and Task Providers use the public comment function to ask questions and offer answers about the published task. Once the Task Provider has accepted an offer by a Droner, the private messaging service of Hireadronepilot can be privately communicated.</li>
+                            <li>Once a Task Provider has accepted an offer from a Droners, it is an obligation on both sides. Del Droners to provide the service and the Task Provider to pay the agreed sum.</li>
+                            <li>Once a Droner has provided the services requested by an Task Provider, as described in the Hireadronepilot, the Task Provider must "finalize" the task inside the Hireadronepilot portal using the "Accept" option and objectively qualify the Droners.</li>
+                            <li>For security, Task Providers should never disclose personal details such as address, phone number, or email address on a posted task or any other public communication on the Hireadronepilot platform.</li>
+                        </ul>
+                        </div>
+</div> --> <!-- this -->
+
+
 </div>
 </div>
 <div class="clear"></div>
